080ad31aafdb4e29214d90a5a02295aa06474452
Thành Trung <npttrung2711@gmail.com>
2024-08-20 15:29:05 +0700

    + init project

---
+++ MessagesClone/App/MessagesCloneApp.swift
@@ -0,0 +1,18 @@
+//
+//  MessagesCloneApp.swift
+//  MessagesClone
+//
+//  Created by Thành Trung on 14/5/24.
+//
+
+import SwiftUI
+
+@main
+struct MessagesCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+//            ContentView()
+            InboxView()
+        }
+    }
+}
---
+++ MessagesClone/Core/Root/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MessagesClone
+//
+//  Created by Thành Trung on 14/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
