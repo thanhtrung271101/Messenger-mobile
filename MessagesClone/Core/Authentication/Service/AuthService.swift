5bed1017fc76a9ad88b095a41ff2e77ab6676814
Thành Trung <npttrung2711@gmail.com>
2024-08-26 17:24:15 +0700

    + upload/download user data

--- MessagesClone/Core/Authentication/Service/AuthService.swift
+++ MessagesClone/Core/Authentication/Service/AuthService.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Firebase
 import FirebaseAuth
+import FirebaseFirestore
 
 class AuthService {
     @Published var userSession: FirebaseAuth.User?
@@ -16,8 +17,10 @@ class AuthService {
     
     init() {
         self.userSession = Auth.auth().currentUser
+        Task { try await UserService.shared.fetchCurrentUser()}
         print("DEBUG: User session id is \(userSession?.uid)")
     }
+    @MainActor
     func login(withEmail email: String, password: String) async throws {
         do {
             let result = try await Auth.auth().signIn(withEmail: email, password: password)
@@ -26,11 +29,12 @@ class AuthService {
             print("DEBUG: Failed to login with error: \(error.localizedDescription)")
         }
     }
-    
+    @MainActor
     func createUser(withEmail email: String, password: String, fullName: String) async throws {
         do {
             let result = try await Auth.auth().createUser(withEmail: email, password: password)
             self.userSession = result.user
+            try await self.uploadUserData(email: email, fullName: fullName, id: result.user.uid)
         } catch {
             print("DEBUG: Failed to create user with error: \(error.localizedDescription)")
         }
@@ -43,5 +47,11 @@ class AuthService {
         } catch {
             print("DEBUG: Failed to sign out with error \(error.localizedDescription)")
         }
+    }
+    
+    func uploadUserData(email: String, fullName: String, id: String) async throws {
+        let user = User(fullName: fullName, email: email, profileImageUrl: nil)
+        guard let encodedUser = try? Firestore.Encoder().encode(user) else { return }
+        try await Firestore.firestore().collection("users").document(id).setData(encodedUser)
     }
 }
