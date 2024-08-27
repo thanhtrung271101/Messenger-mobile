fae378893d0aa1fad45f8c026932afe04b2ceace
Thành Trung <npttrung2711@gmail.com>
2024-08-27 13:13:58 +0700

    + loading current user data

---
+++ MessagesClone/Core/Inbox/ViewModel/InboxViewModel.swift
@@ -0,0 +1,27 @@
+//
+//  InboxViewModel.swift
+//  MessagesClone
+//
+//  Created by Thành Trung on 26/8/24.
+//
+
+import Foundation
+import Combine
+import Firebase
+
+@MainActor
+class InboxViewModel: ObservableObject {
+    @Published var currentUser: User?
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        setupSubscribers()
+    }
+    
+    private func setupSubscribers() {
+        UserService.shared.$currentUser.sink { [weak self] user in
+            self?.currentUser = user
+        }.store(in: &cancellables)
+    }
+}
